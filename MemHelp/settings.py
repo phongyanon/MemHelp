3350740645b2ba54a9bcb327d964efc362809822
phongyanon <phongyanon@gmail.com>
2016-11-06 14:51:04 +0700

    edit settings.py

--- MemHelp/settings.py
+++ MemHelp/settings.py
@@ -122,5 +122,5 @@ STATIC_URL = '/static/'
 
 
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
