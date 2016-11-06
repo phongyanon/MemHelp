64cb75282c335cc37bf5eed3240b9d360f9dde2f
phongyanon <phongyanon@gmail.com>
2016-11-06 15:15:32 +0700

    edit wsgi.py

--- MemHelp/settings.py
+++ MemHelp/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
