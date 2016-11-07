b298bf15517de9aad74895a61668ca7371c681a1
phongyanon <phongyanon@gmail.com>
2016-11-07 21:40:50 +0700

    add runtime.txt

--- MemHelp/wsgi.py
+++ MemHelp/wsgi.py
@@ -14,6 +14,6 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MemHelp.settings")
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
 
 application = DjangoWhiteNoise(application)
